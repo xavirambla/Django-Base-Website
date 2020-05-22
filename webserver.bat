03421c6e654294509a17cf3eab8b0cea63457478
Xavi Rambla <45351293+xavirambla@users.noreply.github.com>
2020-05-22 17:11:46 +0200

    Proyecto Base - Muestra un mensaje en el navegador
    Configura el servidor, se configuran las variables de entorno, algunos ficheros,...

---
+++ webserver.bat
@@ -0,0 +1,10 @@
+set SECRET_KEY= "a5&iq&s1u9h6(1_2%pt2w9)f=c$kso=k$c$w@fi9215s=1q0^d"
+SET ENTORNO = "DEV"
+:: SET ENTORNO = "INT"
+:: SET ENTORNO = "UAT"
+:: SET ENTORNO = "PRD"
+
+python manage.py runserver
+
+pause
+
